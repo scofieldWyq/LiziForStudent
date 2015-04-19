c25c372f2ac663ec208fdbbef236b3fd1e69ca51
scofieldWyq <147036scofield@gmail.com>
2015-04-19 20:21:25 +0800

    Initial Commit

---
+++ LiziForStudent/LiziAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LiziAppDelegate.h
+//  LiziForStudent
+//
+//  Created by 吴英强 on 15/4/19.
+//  Copyright (c) 2015年 吴英强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LiziAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LiziForStudent/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LiziForStudent
+//
+//  Created by 吴英强 on 15/4/19.
+//  Copyright (c) 2015年 吴英强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LiziAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LiziAppDelegate class]));
+    }
+}
